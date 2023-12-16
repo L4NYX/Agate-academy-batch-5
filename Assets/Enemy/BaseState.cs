fe110f8ea83b088d4bb102a7c347dd26114b3b68
L4NYX <137173770+L4NYX@users.noreply.github.com>
2023-12-17 06:36:44 +0700

    EvanEkaKurniawan-Pac-Man_Like
    Player,Camera,level

---
+++ Assets/Enemy/BaseState.cs
@@ -0,0 +1,10 @@
+public interface BaseState
+{
+
+public void EnterState(Enemy enemy);
+
+public void UpdateState(Enemy enemy);
+
+public void ExitState(Enemy enemy);
+
+}
---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.AI;
+
+
+public class Enemy : MonoBehaviour
+
+{
+    private BaseState _currentState;
+
+
+public PatrolState PatrolState = new PatrolState();
+
+public ChaseState ChaseState = new ChaseState();
+
+public RetreatState RetreatState = new RetreatState();
+
+[SerializeField]
+
+public List<Transform> Waypoints = new List<Transform>();
+
+ 
+
+[HideInInspector]
+
+public NavMeshAgent NavMeshAgent;
+
+ 
+
+private void Awake()
+
+{
+
+_currentState = PatrolState;
+
+_currentState.EnterState(this);
+
+NavMeshAgent = GetComponent<NavMeshAgent>();
+
+}
+
+
+private void Update()
+
+{
+
+if(_currentState != null)
+
+{
+
+_currentState.UpdateState(this);
+
+}
+
+}
+
+}
